44551ae156b894963ebf93c675a8ded46b37a81c
NaveenGitRealtek <153925033+NaveenGitRealtek@users.noreply.github.com>
2024-06-25 17:52:33 +0530

    Update get_version.sh

--- get_version.sh
+++ get_version.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-# Read the current version from version.txt
-if [ ! -f version.txt ]; then
-    echo "0.0.1" > version.txt
-fi
-
-CURRENT_VERSION=$(cat version.txt)
-
-# Split the version into its components
-IFS='.' read -r MAJOR MINOR PATCH <<< "$CURRENT_VERSION"
-
-# Increment the PATCH version
-PATCH=$((PATCH + 1))
-
-# Form the new version string
-NEW_VERSION="${MAJOR}.${MINOR}.${PATCH}"
-
-# Update the version.txt with the new version
-echo $NEW_VERSION > version.txt
-
-# Print the new version for use in the workflow
-echo $NEW_VERSION
-
+# Calculate the version number based on the GitHub Actions run number
+VERSION=$(printf "V%0.3d" $GITHUB_RUN_NUMBER)
+echo $VERSION
