6a24d975cfb8dd35bd700cebb15a0237590379d9
NaveenGitRealtek <153925033+NaveenGitRealtek@users.noreply.github.com>
2024-06-25 13:49:48 +0530

    Update get_version.sh

--- get_version.sh
+++ get_version.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-# Calculate the version number based on the GitHub Actions run number
-VERSION=$(printf "V%03d" $GITHUB_RUN_NUMBER)
+# Get the GitHub Actions run number
+RUN_NUMBER=$GITHUB_RUN_NUMBER
+
+# Calculate major, minor, and patch versions
+MAJOR=$((RUN_NUMBER / 10000))
+MINOR=$(((RUN_NUMBER / 100) % 100))
+PATCH=$((RUN_NUMBER % 100))
+
+# Format the version as V x.y.z
+VERSION=$(printf "V %d.%d.%d" $MAJOR $MINOR $PATCH)
 echo $VERSION
+
