158a502b3f75d070f1fbfe18114ecdb5ff42ec37
NaveenGitRealtek <153925033+NaveenGitRealtek@users.noreply.github.com>
2024-06-25 18:12:29 +0530

    Update get_version.sh

--- get_version.sh
+++ get_version.sh
@@ -1,6 +1,11 @@
+# #!/bin/bash
+
+# # Calculate the version number based on the GitHub Actions run number
+# VERSION="V0.0.$GITHUB_RUN_NUMBER"
+# echo $VERSION
+
 #!/bin/bash
 
 # Calculate the version number based on the GitHub Actions run number
 VERSION="V0.0.$GITHUB_RUN_NUMBER"
 echo $VERSION
-
