d394a70a5d96d79a52e0d55dcff5fdc752b7e70b
NaveenGitRealtek <153925033+NaveenGitRealtek@users.noreply.github.com>
2024-06-25 14:46:17 +0530

    Update get_version.sh

--- get_version.sh
+++ get_version.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
-# Define the version file path
-version_file="version.txt"
-
-if [ -e "$version_file" ]; then
-    # Read the last version from the file
-    version=$(cat "$version_file")
-else
-    # If the version file doesn't exist, start with version 0.0.0
-    version="0.0.0"
-fi
-
-# Increment the patch version based on GitHub Actions run number
-# Split the version into major, minor, and patch parts
-major=$(echo "$version" | cut -d '.' -f 1)
-minor=$(echo "$version" | cut -d '.' -f 2)
-patch=$(echo "$version" | cut -d '.' -f 3)
-
-# Calculate the next patch version based on GitHub Actions run number
-next_patch=$((patch + 1))
-
-# Form the new version string
-next_version="V$major.$minor.$next_patch"
-
-# Store the next version in the version file
-echo "$next_version" > "$version_file"
-
-# Output the next version for use in GitHub Actions
-echo "$next_version"
+# Calculate the version number based on the GitHub Actions run number
+VERSION=$(printf "V%03d" $GITHUB_RUN_NUMBER)
+echo $VERSION
